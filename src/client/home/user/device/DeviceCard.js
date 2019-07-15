a8a3e1f580064dc561c19a7af032ac388eb70d0c
nikkokun <mizutaninikkou@gmail.com>
2019-07-16 05:21:03 +0900

    Fix host for production

--- src/client/home/user/device/DeviceCard.js
+++ src/client/home/user/device/DeviceCard.js
@@ -17,7 +17,7 @@ export default class DeviceCard extends Component {
     this.devicename = props.devicename;
     this.devicepass = props.devicepass;
     this.parent = props.parent;
-    this.socket = io.connect('socket.io/');
+    this.socket = io.connect('35.243.93.59:666');
   }
 
   componentDidMount() {
