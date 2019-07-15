8bd801ba4cc46e966029bf5f38386923d82f3d0f
nikkokun <mizutaninikkou@gmail.com>
2019-07-16 04:11:39 +0900

    Fix host for production

--- src/server/index.js
+++ src/server/index.js
@@ -15,9 +15,10 @@ const devices = new Devices();
 
 const app = express();
 app.use(cors());
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Crentials", false);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 app.use(express.static('dist'));
